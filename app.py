02a8e822202a5f5c7705202ed46f0785cedfff29
Abyudaynalawade <151946362+Abyudaynalawade@users.noreply.github.com>
2025-08-15 02:02:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def uploaded_file(filename):
     return os.path.join(UPLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
